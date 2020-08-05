e40d8f354723236888d0296c8f0b666a775b21f4
rushtokunal <rushtokunal@gmail.com>
2020-08-05 17:22:15 -0500

    added DB ops

--- PyFutureOps/__init__.py
+++ PyFutureOps/__init__.py
@@ -1,2 +1,4 @@
 # Version of the PyFutureOps package 
-__version__= "0.0.1"
+__version__= "0.0.3"
+from pyfutureops.database import db_operations,db_tables
+from pyfutureops.examples import multithread_wiki
