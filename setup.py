0a38a9b87570ab447a89ed0c500c00f7e4b8d930
rushtokunal <rushtokunal@gmail.com>
2020-08-05 16:48:13 -0500

    change the base package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'PyFutureOps',         # How you named your package folder (MyLib)
-  packages = ['PyFutureOps'],   # Chose the same as "name"
+  name = 'pyfutureops',         # How you named your package folder (MyLib)
+  packages = ['pyfutureops'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Bootstrap code for Concurrent Futures Batch tracking',   # Give a short description about your library
